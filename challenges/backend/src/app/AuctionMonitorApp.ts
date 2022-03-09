65dac9a2661b5a5ec475f0242c18faf2e31d48b8
Chesvic Lordgape <lordgape@gmail.com>
2022-03-09 13:41:54 +0100

    Integrate service into application

--- challenges/backend/src/app/AuctionMonitorApp.ts
+++ challenges/backend/src/app/AuctionMonitorApp.ts
@@ -2,18 +2,58 @@ import { inject, injectable } from "inversify";
 import { ILogger } from "./services/Logger/interface/ILogger";
 import { DependencyIdentifier } from "./DependencyIdentifiers";
 import "reflect-metadata";
+import { ICarOnSaleClient } from "./services/CarOnSaleClient/interface/ICarOnSaleClient";
+import { IBuyerAuction } from "./services/models/IBuyerAuction";
 
 @injectable()
 export class AuctionMonitorApp {
+  public constructor(
+    @inject(DependencyIdentifier.LOGGER) private logger: ILogger,
+    @inject(DependencyIdentifier.CarOnSaleClient)
+    private carOnSaleClient: ICarOnSaleClient
+  ) {}
 
-    public constructor(
-        @inject(DependencyIdentifier.LOGGER) private logger: ILogger) {
+  public async start(): Promise<void> {
+
+    try {
+      this.logger.log(`Auction Monitor started.`);
+  
+      // TODO: Retrieve auctions and display aggregated information (see README.md)
+      const auctions: IBuyerAuction = await this.carOnSaleClient.getRunningAuctions();
+  
+      const totalNumberOfAuction = auctions?.items?.length;
+      this.logger.log("Number of auctions: " + totalNumberOfAuction);
+  
+      // average number of bids on an auction
+  
+      const averageBid =
+        auctions?.items?.reduce((acc, cur) => (acc += cur.numBids), 0) /
+        (totalNumberOfAuction ? totalNumberOfAuction : 1);
+  
+        this.logger.log("Average number of bids on an auction: " + averageBid);
+  
+      // Average percentage of the auction progress
+  
+      const averageAuctionProgress =
+        auctions?.items?.reduce(
+          (acc, cur) =>
+            (acc += cur.minimumRequiredAsk + cur.currentHighestBidValue),
+          0
+        ) / (totalNumberOfAuction ? totalNumberOfAuction : 1);
+  
+      const avgPercentageAuctionProgress = averageAuctionProgress * 0.01;
+  
+      this.logger.log(
+        "Average percentage of the auction progress: " +
+        avgPercentageAuctionProgress
+      );
+  
+      process.exit(0);
+      
+    } catch (error) {
+      this.logger.log(error.message);
+      
+      process.exit(1);
     }
-
-    public async start(): Promise<void> {
-        this.logger.log(`Auction Monitor started.`);
-
-        // TODO: Retrieve auctions and display aggregated information (see README.md)
-    }
-
+  }
 }
--- challenges/backend/src/app/main.ts
+++ challenges/backend/src/app/main.ts
@@ -1,21 +1,38 @@
-import {Container} from "inversify";
-import {ILogger} from "./services/Logger/interface/ILogger";
-import {Logger} from "./services/Logger/classes/Logger";
-import {DependencyIdentifier} from "./DependencyIdentifiers";
-import {AuctionMonitorApp} from "./AuctionMonitorApp";
+import { Container } from "inversify";
+import { ILogger } from "./services/Logger/interface/ILogger";
+import { Logger } from "./services/Logger/classes/Logger";
+import { DependencyIdentifier } from "./DependencyIdentifiers";
+import { AuctionMonitorApp } from "./AuctionMonitorApp";
+import { IApiClient } from "./services/Api/interface/IApiClient";
+import { ApiClient } from "./services/Api/classes/ApiClient";
+import { ICarOnSaleClient } from "./services/CarOnSaleClient/interface/ICarOnSaleClient";
+import { CarOnSaleClient } from "./services/CarOnSaleClient/classes/CarOnSaleClient";
+
+import * as dotenv from "dotenv";
+const result = dotenv.config();
+if (result.error) {
+  throw result.error;
+}
+
+/*
+ *   Initialize Enivironment
+ */
 
 /*
  * Create the DI container.
  */
 const container = new Container({
-    defaultScope: "Singleton",
+  defaultScope: "Singleton",
 });
 
 /*
  * Register dependencies in DI environment.
  */
 container.bind<ILogger>(DependencyIdentifier.LOGGER).to(Logger);
-
+container.bind<IApiClient>(DependencyIdentifier.APICLIENT).to(ApiClient);
+container
+  .bind<ICarOnSaleClient>(DependencyIdentifier.CarOnSaleClient)
+  .to(CarOnSaleClient);
 
 /*
  * Inject all dependencies in the application & retrieve application instance.
@@ -26,5 +43,7 @@ const app = container.resolve(AuctionMonitorApp);
  * Start the application
  */
 (async () => {
-    await app.start();
+  await app.start();
 })();
+
+export default app;
